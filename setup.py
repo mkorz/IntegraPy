8d1788265ec4f792032c940c2309469a25f76687
Marcin Korzonek <marcin@korzonek.eu>
2017-11-19 19:13:04 +0100

    Updated setup.py to match new layout

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
+#!/usr/bin/env python3
 from distutils.core import setup
-
-version = '1.0'
+from setuptools import find_packages
+version = '1.0.1'
 
 setup(
     name='IntegraPy',
     version=version,
     description=u"Satel's Integra secrity hub interface",
     author='Marcin Korzonek & Michał Węgrzynek & al.',
-    packages=[
-        'IntegraPy'
-    ],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
     include_package_data=True,
     zip_safe=False,
+    license='GNU General Public License',
     install_requires=[
         'bitarray'
     ]
